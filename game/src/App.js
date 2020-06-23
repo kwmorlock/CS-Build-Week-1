a99c8a77c6844e6f6f97791fa927bf517b6c04cb
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-23 18:05:45 -0400

    default death setup

--- game/src/App.js
+++ game/src/App.js
@@ -3,9 +3,15 @@ import React, {useState} from 'react';
 // import './App.css';
 import HomeScreen from "./components/homescreen";
 
+const numberRows = 100;
+const numberColumns = 100;
+
 function App() {
   const [board, setBoard] = useState(() => {
     const rows = [];
+    for (let i = 0; i < numberRows; i++){ //rows for the grid
+    rows.push(Array.from(Array(numberColumns), () => 0)) //pushing a column which is an array, by default everything will be dead which is why 0, and if 1 that means its alive
+    }
   })
   return (
     <div className="App">
