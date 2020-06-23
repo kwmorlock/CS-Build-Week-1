eb9413edefa157fbfdabde0f59b746a496c3e5c5
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-23 17:32:45 -0400

    useState added to app.js

--- game/src/App.js
+++ game/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import HomeScreen from "./components/homescreen";
 
 function App() {
+  const [board, setBoard] = useState(() => {
+    const rows = [];
+  })
   return (
     <div className="App">
  <HomeScreen />
